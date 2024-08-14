b6ec248d0c23d1a492af9cc88d1e0a44d34eac7e
saujalkarki <saujalkarki04@gmail.com>
2024-08-14 23:45:30 +0545

    some main changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "strict mode";
+
+const hambuger_dot = document.querySelector(".hamburger");
+const ham_wrapper = document.querySelector(".ham-wrapper");
+
+hambuger_dot.addEventListener("click", function () {
+  console.log("clicked");
+  hambuger_dot.classList.toggle("active");
+  ham_wrapper.classList.toggle("active");
+});
